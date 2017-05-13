a30b2827420afbfa91531259e7ab9f9dac4bb541
bigrocs <bigrocs@qq.com>
2017-05-13 10:24:31 +0800

    coreCMF admin packages inti

---
+++ src/adminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CoreCMF\admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class adminServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
