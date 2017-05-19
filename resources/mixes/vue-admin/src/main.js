2ec96a7504149b30cc22faf0299edc90d46cd444
bigrocs <bigrocs@qq.com>
2017-05-19 16:31:42 +0800

    删除无用组件文件引用builder-vue-iview包开发

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,19 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Vuex from 'vuex'
-import axios from 'axios'
+import {App, Store} from 'builder-vue-iview'
+import VueRouter from 'vue-router'
+import Axios from 'axios'
+console.log(App)
+Vue.use(VueRouter)
 
-Vue.use(Vuex)
-
-Vue.http = axios
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
+/* 设置api通信url */
+Store.state.apiUrl = window.config.apiUrl
 /* eslint-disable no-new */
+const router = new VueRouter({
+  routes: []
+})
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  Store,
+  render: h => h(App)
 })
