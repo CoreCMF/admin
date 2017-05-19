fad160c0fe53062cf65b65d2be3d32a9019a0bb7
bigrocs <bigrocs@qq.com>
2017-05-19 17:20:28 +0800

    修复BUG store

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,16 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import {App, Store} from 'builder-vue-iview'
+import {App, store} from 'builder-vue-iview'
 import VueRouter from 'vue-router'
 import Axios from 'axios'
-console.log(App)
+
 Vue.use(VueRouter)
 
 Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 /* 设置api通信url */
-Store.state.apiUrl = window.config.apiUrl
+store.state.apiUrl = window.config.apiUrl
 /* eslint-disable no-new */
 const router = new VueRouter({
   routes: []
@@ -18,6 +18,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
-  Store,
+  store,
   render: h => h(App)
 })
