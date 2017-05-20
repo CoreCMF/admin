cf97ee5d57a851d380b64af374489bfaaa1245ba
bigrocs <bigrocs@qq.com>
2017-05-20 09:30:17 +0800

    把路由初始化扔到builder-vue包内

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,20 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import {App, store} from 'builder-vue-iview'
-import VueRouter from 'vue-router'
+import {App, router, store} from 'builder-vue'
 import Axios from 'axios'
-
-Vue.use(VueRouter)
-
+console.log(router)
 Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 /* 设置api通信url */
 store.state.apiUrl = window.config.apiUrl
 /* eslint-disable no-new */
-const router = new VueRouter({
-  routes: []
-})
 new Vue({
   el: '#app',
   router,
