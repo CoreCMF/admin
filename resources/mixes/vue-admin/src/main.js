4f1006f347e59f25773e0f7e5f4dc748de0836cb
bigrocs <bigrocs@qq.com>
2017-05-20 17:06:53 +0800

    增加容器组件index

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import {App, router, store} from 'builder-vue'
-import builderVueIview from 'builder-vue-iview'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import BuilderVueIview from 'builder-vue-iview'
+import Index from './components/index.vue'
 import Axios from 'axios'
 
-Vue.use(builderVueIview)
+Vue.use(iView)
+Vue.use(BuilderVueIview)
 Vue.prototype.$http = Axios
 
 /* 设置api通信url */
 store.state.apiUrl = window.config.apiUrl
 /* 容器组件 */
-store.state.container = ''
+store.state.container = Index
 /* builder索引组件 */
-store.state.builderIndex = '<Bvi-index></Bvi-index>'
+store.state.builderIndex = { template: '<Bvi-index></Bvi-index>' }
 
 /* eslint-disable no-new */
 new Vue({
