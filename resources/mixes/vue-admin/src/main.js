9f153e5070cb6cb37a00aa082974ddfbff3edd6c
bigrocs <bigrocs@qq.com>
2017-05-20 16:45:07 +0800

    增加 builderIndex

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,16 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import {App, router, store} from 'builder-vue'
 import builderVueIview from 'builder-vue-iview'
 import Axios from 'axios'
 
 Vue.use(builderVueIview)
-
-Vue.config.productionTip = false
 Vue.prototype.$http = Axios
+
 /* 设置api通信url */
 store.state.apiUrl = window.config.apiUrl
+/* 容器组件 */
+store.state.container = ''
+/* builder索引组件 */
+store.state.builderIndex = '<Bvi-index></Bvi-index>'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
