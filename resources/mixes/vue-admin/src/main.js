914364013a7002bf7df9ad3c969988843b662f9f
bigrocs <bigrocs@qq.com>
2017-05-20 16:01:21 +0800

    注册builderVueIview

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import {App, router, store} from 'builder-vue'
+import builderVueIview from 'builder-vue-iview'
 import Axios from 'axios'
+
+Vue.use(builderVueIview)
 
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios
