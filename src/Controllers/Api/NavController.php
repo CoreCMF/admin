468a68bb43e36dfc1bbe71b9ae0ba8bdeb23588a
bigrocs <bigrocs@qq.com>
2017-06-07 15:35:13 +0800

    添加sidebar 数据获取处理等

--- src/Controllers/Api/NavController.php
+++ src/Controllers/Api/NavController.php
@@ -11,11 +11,13 @@ class NavController extends Controller
 {
     private $builderMain;
     private $menuModel;
-    private $group = 'admin';
+    private $name;
     /** return  CoreCMF\core\Builder\Main */
-    public function __construct(Menu $MenuPr)
+    public function __construct(Menu $MenuPro)
     {
+        $this->menuModel = $MenuPro;
         $this->builderMain = resolve('builderAdminMain');        //全局统一实例
+        $this->name = config('admin.topNav.name');
     }
     public function top()
     {
@@ -24,12 +26,16 @@ class NavController extends Controller
         $topNav['apiUrl'] = route('api.admin.nav.sidebar');
 
         $builderMain = $this->builderMain;
-        $builderMain->topNavDefaultActive(config('admin.topNav.name'));
-        $builderMain->topNavList($topNav);
+        $builderMain->topNavDefaultActive($this->name);
+        $builderMain->addTopNavList($topNav);
         return $builderMain->getTopNavs();
     }
     public function sidebar()
     {
-        dd('sidebar');
+        $sidebar = $this->menuModel->getGroupMenus($this->name);
+
+        $builderMain = $this->builderMain;
+        $builderMain->setMenus($sidebar);
+        return $builderMain->getMenus();
     }
 }
