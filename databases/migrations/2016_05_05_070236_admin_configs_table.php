9c4d1547b04fcb7d0a25368fd6b6fbaf0bf4b788
bigrocs <bigrocs@qq.com>
2017-06-12 10:17:03 +0800

    增加config迁移文件盒填充文件

---
+++ databases/migrations/2016_05_05_070236_admin_configs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdminConfigsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_configs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')         ->comment('配置标题');
+            $table->string('name')          ->comment('配置名称');
+            $table->text('value')           ->comment('配置值')->nullable();
+            $table->tinyInteger('group')    ->comment('配置分组')->unsigned();
+            $table->string('type')          ->comment('配置类型');
+            $table->string('icon')          ->comment('配置字体图标')->nullable();
+            $table->string('options')       ->comment('配置额外值')->nullable();
+            $table->string('placeholder')           ->comment('配置说明')->nullable();
+            $table->tinyInteger('status')   ->comment('状态')->default(1);
+            $table->bigInteger('sort')      ->comment('排序')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_configs');
+    }
+}
--- databases/seeds/AdminMenuTableSeeder.php
+++ databases/seeds/AdminMenuTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class MenuTableSeeder extends Seeder
+class AdminMenuTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
