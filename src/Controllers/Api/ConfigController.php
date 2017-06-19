0a55bcd363c0e54b102800f81a0bfe7813b90609
bigrocs <bigrocs@qq.com>
2017-06-19 17:30:18 +0800

    add table data  column topButton rightButton response

--- src/Controllers/Api/ConfigController.php
+++ src/Controllers/Api/ConfigController.php
@@ -24,20 +24,32 @@ class ConfigController extends Controller
         $group = $request->tabIndex;
         $group = empty($group) ? 0 : $group;
         $configs = $this->configModel
-                            // ->where('group', '=', $group)
+                            ->where('group', '=', $group)
                             ->orderBy('sort', 'ASC')
                             ->get();
         $tabs = $this->configModel->tabsConfigGroupList();
 
-        $form = resolve('builderForm')
-                  ->data($configs)
+        $table = resolve('builderTable')
                   ->tabs($tabs)
                   ->tabsGroup('group')
-                  ->apiUrl('submit',route('api.admin.system.system.update'))
-                  ->config('labelWidth','100px');
+                  ->data($configs)
+                  ->column(['prop' => 'id',         'label'=> 'ID',     'width'=> '55'])
+                  ->column(['prop' => 'name',       'label'=> '名称',   'minWidth'=> '240'])
+                  ->column(['prop' => 'title',      'label'=> '标题',   'minWidth'=> '180'])
+                  ->column(['prop' => 'sort',       'label'=> '排序',   'width'=> '70'])
+                  ->column(['prop' => 'status',     'label'=> '状态',   'minWidth'=> '90','type' => 'status'])
+                  ->column(['prop' => 'rightButton','label'=> '操作',   'minWidth'=> '220','type' => 'btn'])
+                  ->topButton(['buttonType'=>'add',       'apiUrl'=> route('api.admin.system.config.add'),'title'=>'添加配置'])                         // 添加新增按钮
+                  ->topButton(['buttonType'=>'resume',    'apiUrl'=> route('api.admin.system.config.status')])                         // 添加启用按钮
+                  ->topButton(['buttonType'=>'forbid',    'apiUrl'=> route('api.admin.system.config.status')])                         // 添加禁用按钮
+                  ->topButton(['buttonType'=>'delete',    'apiUrl'=> route('api.admin.system.config.delete')])                         // 添加删除按钮
+                  ->rightButton(['buttonType'=>'edit',    'apiUrl'=> route('api.admin.system.config.edit')])                         // 添加编辑按钮
+                  ->rightButton(['buttonType'=>'forbid',  'apiUrl'=> route('api.admin.system.config.status')])                       // 添加禁用/启用按钮
+                  ->rightButton(['buttonType'=>'delete',  'apiUrl'=> route('api.admin.system.config.delete')])                       // 添加删除按钮
+                ;
         $html = resolve('builderHtml')
-                  ->title('系统设置')
-                  ->item($form)
+                  ->title('配置管理')
+                  ->item($table)
                   ->itemConfig('layout',['xs' => 24, 'sm' => 20, 'md' => 18, 'lg' => 16])
                   ->response();
         return $html;
