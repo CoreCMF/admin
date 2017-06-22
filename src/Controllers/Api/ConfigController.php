9c270b8e47ca87ff833f0dfbc98c443db5e2b8ad
bigrocs <bigrocs@qq.com>
2017-06-22 09:32:35 +0800

    add delete store edit update

--- src/Controllers/Api/ConfigController.php
+++ src/Controllers/Api/ConfigController.php
@@ -60,8 +60,18 @@ class ConfigController extends Controller
             $config = $this->configModel->where('id', '=', $id)->update(['status' => $value]);
         }
         $data = [
-                    'title'     => '状态已更改',
                     'message'   => '后台配置数据状态更改成功!',
+                    'type'      => 'success',
+                ];
+        return response()->json($data, 200);
+    }
+    public function delete(Request $request){
+        $input = $request->all();
+        foreach ($input as $id => $value) {
+            $response = $this->configModel->find($id)->forceDelete();
+        }
+        $data = [
+                    'message'   => '后台配置数据删除成功!',
                     'type'      => 'success',
                 ];
         return response()->json($data, 200);
@@ -87,4 +97,65 @@ class ConfigController extends Controller
                   ->response();
         return $html;
     }
+    /**
+     * [store 新增配置数据].
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $response = $this->configModel->create($input);
+        if ($response->wasRecentlyCreated) {
+            $data = [
+                        'message'   => '新增配置数据成功！!',
+                        'type'      => 'success',
+                    ];
+        }else{
+            $data = [
+                        'message'   => '新增配置数据失败！!',
+                        'type'      => 'error',
+                    ];
+        }
+        return response()->json($data, 200);
+    }
+    public function edit(Request $request){
+        $config = $this->configModel->find($request->id);
+        $configGroupList = $this->configModel->tabsConfigGroupList();
+        $itemType = resolve('builderForm')->itemType;
+        $form = resolve('builderForm')
+                ->item(['name' => 'group',     'type' => 'select',   'label' => '配置分组',     'placeholder' => '配置所属的分组',                          'options'=>$configGroupList,     'value'=>0])
+                ->item(['name' => 'type',      'type' => 'select',   'label' => '配置类型',     'placeholder' => '配置类型的分组',                          'options'=>$itemType,            'value'=>'text'])
+                ->item(['name' => 'name',      'type' => 'text',     'label' => '配置名称',     'placeholder' => '配置名称'])
+                ->item(['name' => 'title',     'type' => 'text',     'label' => '配置标题',     'placeholder' => '配置标题'])
+                ->item(['name' => 'value',     'type' => 'textarea', 'label' => '配置值',       'placeholder' => '配置值',                                   'rows'=>4])
+                ->item(['name' => 'options',   'type' => 'textarea', 'label' => '配置项',       'placeholder' => '如果是单选、多选、下拉等类型 需要配置该项',      'rows'=>4])
+                ->item(['name' => 'tip',       'type' => 'textarea', 'label' => '配置说明',     'placeholder' => '配置说明',                                  'rows'=>4])
+                ->item(['name' => 'sort',      'type' => 'number',   'label' => '排序',         'placeholder' => '用于显示的顺序',                             'value'=>0])
+                ->itemData($config)
+                ->apiUrl('submit',route('api.admin.system.config.store'))
+                ->config('labelWidth','100px');
+        $html = resolve('builderHtml')
+                  ->title('编辑配置')
+                  ->item($form)
+                  ->itemConfig('layout',['xs' => 24, 'sm' => 20, 'md' => 18, 'lg' => 16])
+                  ->response();
+        return $html;
+    }
+    public function update(Request $request)
+    {
+        $input = $request->all();
+        $response = $this->adminConfigModel->find($input['id'])->fill($input)->save();
+        if ($response) {
+            $data = [
+                        'message'   => '编辑配置数据成功！!',
+                        'type'      => 'success',
+                    ];
+        }else{
+            $data = [
+                        'message'   => '编辑配置数据失败！!',
+                        'type'      => 'error',
+                    ];
+        }
+        return response()->json($data, 200);
+    }
+
 }
