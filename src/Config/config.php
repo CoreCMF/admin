6aac966d3a4ca2cda2adf8b33597397560f3bf31
bigrocs <bigrocs@qq.com>
2017-07-06 10:03:25 +0800

    add

---
+++ src/Config/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'name' => 'admin',
+	'description' => 'corecmf admin package',
+	'author' => 'bigRocs',
+
+	'topNav' => [
+		'name'	=> 'admin',
+		'title' => '系统',
+		'icon' => 'fa fa-cog',
+		'sort' => 0,
+	],
+];
