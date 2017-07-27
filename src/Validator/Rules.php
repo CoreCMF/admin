6a8da0c5643634a0dd9c321e988d554dbb773b7d
bigrocs <bigrocs@qq.com>
2017-07-27 16:09:30 +0800

    设定验证规则

--- src/Validator/Rules.php
+++ src/Validator/Rules.php
@@ -5,48 +5,62 @@ namespace CoreCMF\admin\Validator;
 use CoreCMF\core\Support\Validator\Rules as coreRules;
 class Rules extends coreRules
 {
-    public function admin(){
-        $this->mobile('请输入管理员手机号码');
+    public function getRules(){
+        $mobilePassword = "
+            (rule, value, callback) => {
+                if (value === '') {
+                  callback(new Error('请输入手机号码'));
+                } else {
+                    if (!/^1[3578]\d{9}$/.test(value)) {
+                        callback(new Error('请输入正确的手机号码'));
+                    }
+                    callback();
+                }
+            }
+        ";
+        $validatePassword = "
+            (rule, value, callback) => {
+                if (value === '') {
+                  callback();
+                } else {
+                    if (!/^.{6,16}$/.test(value)) {
+                        callback(new Error('密码长度在 6 到 16 个字符'));
+                    }
+                    if (this.fromDatas.checkPassword !== '') {
+                        this.\$refs.fromDatas.validateField('checkPassword');
+                    }
+                  callback();
+                }
+            }";
+        $validateCheckPassword = "
+            (rule, value, callback) => {
+                if (value !== this.fromDatas.password) {
+                  callback(new Error('两次输入密码不一致!'));
+                } else {
+                  callback();
+                }
+            }";
         return [
-            'admin_account'=> [
-                ['required' => true,  'message' => '请输入管理员账号', 'trigger'=> 'blur'],
-                [ 'min' => 4, 'max' => 16, 'message' => '长度在 4 到 16 个字符', 'trigger' => 'blur' ]
+            'name'=> [
+                ['required' => true,  'message' => '请输入用户名', 'trigger'=> 'blur'],
+                [ 'min'=> 4, 'max'=> 10, 'message'=> '长度在 4 到 10 个字符', 'trigger'=> 'blur' ]
             ],
-            'admin_password'=> [
-                [ 'required'=> true, 'message'=> '请输入管理员密码', 'trigger'=> 'blur' ],
-                [ 'min' => 6, 'max' => 16, 'message' => '长度在 6 到 16 个字符', 'trigger' => 'blur' ]
+            'email'=> [
+                [ 'required'=> true, 'message'=> '请输入邮箱地址', 'trigger'=> 'blur' ],
+                [ 'type'=> 'email', 'message'=> '请输入正确的邮箱地址', 'trigger'=> 'blur,change' ]
             ],
-            'admin_email'=> [
-                ['required' => true,  'message' => '请输入管理员邮箱', 'trigger'=> 'blur'],
-                [ 'type' => 'email', 'message' => '请输入正确的邮箱地址', 'trigger' => 'blur,change' ]
+            'mobile'=> [
+                [ 'required'=> true, 'validator'=> $mobilePassword,'trigger'=> 'blur' ],
+                // [ 'min'=> 11, 'max'=> 11, 'type'=> 'number', 'message'=> '请输入正确的手机号码', 'trigger'=> 'blur,change' ]
             ],
-            'admin_mobile'=> [
-                [ 'required'=> true, 'validator'=> $this->mobile,'trigger'=> 'blur' ]
+            'password'=> [
+                [ 'validator'=> $validatePassword, 'trigger'=> 'blur']
             ],
-        ];
-    }
-    public function database(){
-        return [
-            'sitename'=> [
-                ['required' => true,  'message' => '请输入网站名称', 'trigger'=> 'blur']
+            'checkPassword'=> [
+                [ 'validator'=> $validateCheckPassword, 'trigger'=> 'blur', 'checkMessage'=>'两次输入密码不一致!' ]
             ],
-            'database_engine'=> [
-                [ 'required'=> true, 'message'=> '请选择数据库引擎', 'trigger'=> 'blur' ]
-            ],
-            'database_host'=> [
-                ['required' => true,  'message' => '请输入数据库地址', 'trigger'=> 'blur']
-            ],
-            'database_port'=> [
-                [ 'required'=> true, 'message'=> '请输入数据库端口', 'trigger'=> 'blur' ]
-            ],
-            'database_name'=> [
-                ['required' => true,  'message' => '请输入数据库名称', 'trigger'=> 'blur']
-            ],
-            'database_username'=> [
-                [ 'required'=> true, 'message'=> '请输入数据库用户名', 'trigger'=> 'blur' ]
-            ],
-            'database_password'=> [
-                ['required' => true,  'message' => '请输入数据库密码', 'trigger'=> 'blur']
+            'avatar'=> [
+                [ 'required'=> true, 'message'=> '请上传头像' ],
             ],
         ];
     }
