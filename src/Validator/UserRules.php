89017d3b2a711123f491f67a1f2ff3bd174a5184
bigrocs <bigrocs@qq.com>
2017-07-28 17:20:52 +0800

    add editUser

--- src/Validator/UserRules.php
+++ src/Validator/UserRules.php
@@ -11,7 +11,9 @@ class UserRules extends coreRules
                 if (value == undefined) {
                   callback('请输入用户名');
                 } else {
-                  ".$this->asyncField(route('api.admin.system.user.check'),'name')."
+                  ".$this->asyncField(route('api.admin.system.user.check'),'{
+                    name:this.fromData.name
+                  }')."
                 }
             }
         ";
@@ -20,7 +22,9 @@ class UserRules extends coreRules
                 if (value == undefined) {
                   callback('请输入邮箱地址');
                 } else {
-                  ".$this->asyncField(route('api.admin.system.user.check'),'email')."
+                  ".$this->asyncField(route('api.admin.system.user.check'),'{
+                    email:this.fromData.email
+                  }')."
                 }
             }
         ";
@@ -32,7 +36,9 @@ class UserRules extends coreRules
                     if (!/^1[3578]\d{9}$/.test(value)) {
                       callback('请输入正确的手机号码');
                     }else{
-                      ".$this->asyncField(route('api.admin.system.user.check'),'mobile')."
+                      ".$this->asyncField(route('api.admin.system.user.check'),'{
+                        mobile:this.fromData.mobile
+                      }')."
                     }
                 }
             }
@@ -63,4 +69,94 @@ class UserRules extends coreRules
             ],
         ];
     }
+    public function editUser(){
+        $name = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback('请输入用户名');
+                } else {
+                  ".$this->asyncField(route('api.admin.system.user.check'),'{
+                    name:this.fromData.name,
+                    id:this.fromData.id
+                  }')."
+                }
+            }
+        ";
+        $email = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback('请输入邮箱地址');
+                } else {
+                  ".$this->asyncField(route('api.admin.system.user.check'),'{
+                    email:this.fromData.email,
+                    id:this.fromData.id
+                  }')."
+                }
+            }
+        ";
+        $mobile = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback('请输入手机号码');
+                } else {
+                    if (!/^1[3578]\d{9}$/.test(value)) {
+                      callback('请输入正确的手机号码');
+                    }else{
+                      ".$this->asyncField(route('api.admin.system.user.check'),'{
+                        mobile:this.fromData.mobile,
+                        id:this.fromData.id
+                      }')."
+                    }
+                }
+            }
+        ";
+        $password = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback();
+                } else {
+                    if (!/^.{6,16}$/.test(value)) {
+                        callback('密码长度请控制在 6 到 16 个字符');
+                    }
+                    if (this.fromData.checkPassword !== '') {
+                        this.\$refs.bvefrom.validateField('checkPassword');
+                    }
+                  callback();
+                }
+            }";
+        $checkPassword = "
+                (rule, value, callback) => {
+                    if (value !== this.fromData.password) {
+                      callback('两次输入密码不一致!');
+                    } else {
+                      callback();
+                    }
+                }";
+        return [
+            'name'=> [
+                [ 'min'=> 4, 'max'=> 10, 'message'=> '长度在 4 到 10 个字符', 'trigger'=> 'blur' ],
+                ['required' => true,  'validator' => $name, 'trigger'=> 'blur'],
+            ],
+            'email'=> [
+                [ 'type'=> 'email', 'message'=> '请输入正确的邮箱地址', 'trigger'=> 'blur,change' ],
+                [ 'required'=> true, 'validator'=> $email, 'trigger'=> 'blur' ]
+            ],
+            'mobile'=> [
+                [ 'required'=> true, 'validator'=> $mobile,'trigger'=> 'blur' ]
+            ],
+            'password'=> [
+                [ 'validator'=> $password , 'trigger'=> 'blur']
+            ],
+            'checkPassword'=> [
+                [ 'validator'=> $checkPassword, 'trigger'=> 'blur' ]
+            ],
+            'roles'=> [
+              [ 'required' => true, 'type' => 'array', 'message' => '请至少选择一个用户角色', 'trigger' => 'change' ]
+            ],
+            'avatar'=> [
+                [ 'required'=> true, 'message'=> '请上传头像' ],
+            ],
+        ];
+    }
+
 }
