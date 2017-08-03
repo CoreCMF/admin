99243f37c4f743c74c7841e6c0440e772240d98c
bigrocs <bigrocs@qq.com>
2017-08-03 08:51:33 +0800

    后台角色填充移动到 admin 组件中国年

---
+++ src/Databases/seeds/RoleTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+namespace CoreCMF\admin\Databases\seeds;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('entrust_roles')->insert([
+            'name' 	            => 'admin',
+            'display_name' 	    => '超级管理员',
+            'description' 	    => '网站超级管理员,具有最高权限',
+            'group'             => 'admin'
+        ]);
+    }
+}
