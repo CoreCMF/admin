88d052c4cd26f485ab20f594efe3b45939018bca
bigrocs <bigrocs@qq.com>
2018-01-05 18:42:15 +0800

    vue2.5 tag init

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -1,27 +1,39 @@
+
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import {App, router, store} from 'builder-vue'
-import ElementUI from 'element-ui'
-import BuilderVueElement from 'builder-vue-element'
-import ContainerVueElement from 'container-vue-element'
-window.Vue = Vue
-window.axios = require('axios')
 
-window.axios.defaults.headers.common = {
-  'X-CSRF-TOKEN': window.config.csrfToken,
-  'X-Requested-With': 'XMLHttpRequest'
-}
-
-Vue.use(ElementUI)
-Vue.use(BuilderVueElement)
-Vue.use(ContainerVueElement)
-
-/* 设置api通信url */
-store.state.apiUrl = window.config.apiUrl
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+  el: '#app'
 })
+
+// import Vue from 'vue'
+// import {App, router, store} from 'builder-vue'
+// import ElementUI from 'element-ui'
+// import BuilderVueElement from 'builder-vue-element'
+// import ContainerVueElement from 'container-vue-element'
+// window.Vue = Vue
+// window.axios = require('axios')
+//
+// window.axios.defaults.headers.common = {
+//   'X-CSRF-TOKEN': window.config.csrfToken,
+//   'X-Requested-With': 'XMLHttpRequest'
+// }
+//
+// Vue.use(ElementUI)
+// Vue.use(BuilderVueElement)
+// Vue.use(ContainerVueElement)
+//
+// /* 设置api通信url */
+// store.state.apiUrl = window.config.apiUrl
+//
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   render: h => h(App)
+// })
