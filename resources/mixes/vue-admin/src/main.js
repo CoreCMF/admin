5102f87457ad0ae5a8c78a54bb2f962953747b88
bigrocs <bigrocs@qq.com>
2018-01-06 07:34:26 +0800

    update

--- resources/mixes/vue-admin/src/main.js
+++ resources/mixes/vue-admin/src/main.js
@@ -2,12 +2,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import {App, router, store} from 'builder-vue'
+
+window.Vue = Vue
+window.axios = require('axios')
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app'
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
 })
 
 // import Vue from 'vue'
