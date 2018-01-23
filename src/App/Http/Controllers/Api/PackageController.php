1576194f22bb723816ba69b3afddcf2dcb25a5c8
bigrocs <bigrocs@qq.com>
2018-01-23 17:09:14 +0800

    通过show hide 控制状态按钮

--- src/App/Http/Controllers/Api/PackageController.php
+++ src/App/Http/Controllers/Api/PackageController.php
@@ -40,19 +40,18 @@ class PackageController extends Controller
                                     ->column(['prop' => 'name',       'label'=> '标识',   'minWidth'=> '120'])
                                     ->column(['prop' => 'title',      'label'=> '名称',   'minWidth'=> '180'])
                                     ->column(['prop' => 'status',     'label'=> '状态',   'minWidth'=> '90','type' => 'status', 'config'=> $this->packageModel->status])
-                                    // ->column(['prop' => 'description','label'=> '描述',   'width'=> '180'])
                                     ->column(['prop' => 'author',     'label'=> '作者',   'minWidth'=> '100'])
                                     ->column(['prop' => 'version',    'label'=> '版本',   'minWidth'=> '100'])
                                     ->column(['prop' => 'rightButton','label'=> '操作',   'minWidth'=> '220','type' => 'btn'])
 
-                                    // ->topButton(['buttonType'=>'add',       'apiUrl'=> route('api.admin.app.package.add'),'title'=>'安装扩展包'])                         // 添加新增按钮
                                     ->topButton(['buttonType'=>'resume',    'apiUrl'=> route('api.admin.app.package.status')])                         // 添加启用按钮
                                     ->topButton(['buttonType'=>'forbid',    'apiUrl'=> route('api.admin.app.package.status')])                         // 添加禁用按钮
                                     ->topButton(['buttonType'=>'delete',    'apiUrl'=> route('api.admin.app.package.delete'),'title'=>'卸载'])                         // 添加删除按钮
 
-                                    ->rightButton(['buttonType'=>'add',     'apiUrl'=> route('api.admin.app.package.add'),'title'=>'安装'])                       // 添加删除按钮
-                                    ->rightButton(['buttonType'=>'forbid',  'apiUrl'=> route('api.admin.app.package.status')])                                      // 添加禁用/启用按钮
-                                    ->rightButton(['buttonType'=>'delete',  'apiUrl'=> route('api.admin.app.package.delete'),'title'=>'卸载'])                       // 添加删除按钮
+                                    ->rightButton(['buttonType'=>'add',     'apiUrl'=> route('api.admin.app.package.add'),'title'=>'安装', 'show'=> ['uninstall']])                       // 添加删除按钮
+                                    ->rightButton(['buttonType'=>'forbid',  'apiUrl'=> route('api.admin.app.package.status'), 'show'=>['open']])    //禁用
+                                    ->rightButton(['buttonType'=>'resume',  'apiUrl'=> route('api.admin.app.package.status'), 'show'=>['off']])                                     //启动
+                                    ->rightButton(['buttonType'=>'delete',  'apiUrl'=> route('api.admin.app.package.delete'),'title'=>'卸载', 'hide'=>['uninstall']])                       // 添加删除按钮
                                     ->pagination(['total'=>$data['total'], 'pageSize'=>$data['pageSize'], 'pageSizes'=>$pageSizes])
                                     ->searchTitle('请输入搜索内容')
                                     ->searchSelect(['id'=>'ID','name'=>'标识','title'=>'名称','author'=>'作者'])
