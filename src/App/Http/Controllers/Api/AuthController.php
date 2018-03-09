b08f7de84ff30baadcb575d6502209bb422189fb
bigrocs <bigrocs@qq.com>
2018-03-09 14:18:49 +0800

    get token

--- src/App/Http/Controllers/Api/AuthController.php
+++ src/App/Http/Controllers/Api/AuthController.php
@@ -3,28 +3,22 @@
 namespace CoreCMF\Admin\App\Http\Controllers\Api;
 
 use Auth;
-use Illuminate\Http\Response;
+use CoreCMF\Core\App\Models\PassportClient;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Laravel\Passport\ApiTokenCookieFactory;
 
 class AuthController extends Controller
 {
-    /**
-     * The API token cookie factory instance.
-     *
-     * @var ApiTokenCookieFactory
-     */
-    protected $cookieFactory;
+    protected $passportClient;
     /**
      * Create a new middleware instance.
      *
      * @param  ApiTokenCookieFactory  $cookieFactory
      * @return void
      */
-    public function __construct(ApiTokenCookieFactory $cookieFactory)
+    public function __construct(PassportClient $passportClientRepo)
     {
-        $this->cookieFactory = $cookieFactory;
+        $this->passportClient = $passportClientRepo;
     }
     public function index()
     {
@@ -46,7 +40,7 @@ class AuthController extends Controller
               ->item(['name' => 'username',      'type' => 'text',     'placeholder' => '用户名/手机/邮箱'])
               ->item(['name' => 'password',      'type' => 'password',    'placeholder' => '请输入账户密码'])
               ->rules($rules)
-              ->apiUrl('submit', route('admin.auth.login'))
+              ->apiUrl('submit', route('api.admin.auth.token'))
               ->config('redirect', env('APP_URL').'/admin/dashboard')
               ->config('formStyle', [ 'width'=>'300px', 'padding'=>'20px 10px' ])
               ->config('formSubmit', [ 'name'=>'登陆', 'style'=> ['width'=>'100%'] ])
@@ -54,8 +48,40 @@ class AuthController extends Controller
               ->config('labelWidth', '0');
         return resolve('builderHtml')->title('后台登陆')->item($form)->response();
     }
-    public function getToken()
+    /**
+     * [getToken description]
+     * @return   [type]         [description]
+     * @Author   bigrocs
+     * @QQ       532388887
+     * @Email    bigrocs@qq.com
+     * @DateTime 2018-03-09
+     */
+    public function getToken(Request $request)
     {
+        $token = $this->passportClient->getPasswordToken($request->username, $request->password);
+        if (!empty($token['statusCode'])) {
+            switch ($token['statusCode']) {
+                case 401:
+                    $message = [
+                            'message'   => '登录失败！请检查账号密码是否正确!',
+                            'type'      => 'warning'
+                        ];
+                    break;
+                default:
+                    $message = [
+                            'message'   => '登录失败!未知错误。',
+                            'type'      => 'error'
+                        ];
+                    break;
+            }
+        } else {
+            $token['statusCode'] = 200;
+            $message = [
+                   'message'   => '登录已成功！正在跳转请稍后!',
+                   'type'      => 'success',
+               ];
+        }
+        return resolve('builderHtml')->config('token', $token)->message($message)->response();
     }
     public function authCheck()
     {
@@ -81,40 +107,6 @@ class AuthController extends Controller
                 ];
         }
         return resolve('builderHtml')->auth($auth)->message($message)->response();
-    }
-    public function postLogin(Request $request, Response $response)
-    {
-        $cookie = null;
-        $email = ['email' => $request->username, 'password' => $request->password];
-        $mobile = ['mobile' => $request->username, 'password' => $request->password];
-        $name = ['name' => $request->username, 'password' => $request->password];
-        if (Auth::attempt($email) || Auth::attempt($mobile) || Auth::attempt($name)) {
-            if (Auth::user()->hasGroup('admin')) {
-                //设置Passport认证Cookie
-                $cookie = $this->cookieFactory->make(
-                   Auth::id(),
-                   $request->session()->token()
-               );
-                $auth = true;
-                $message = [
-                       'message'   => '登录已成功！正在跳转请稍后!',
-                       'type'      => 'success',
-                   ];
-            } else {
-                $auth = false;
-                $message = [
-                      'message'   => '登录失败！您没有后台管理权限!',
-                      'type'      => 'warning'
-                  ];
-            }
-        } else {
-            $auth = false;
-            $message = [
-                    'message'   => '登录失败！请检查账号密码是否正确!',
-                    'type'      => 'error'
-                ];
-        }
-        return resolve('builderHtml')->auth($auth)->message($message)->cookie($cookie)->response();
     }
     /**
      * [postLogout 用户退出]
